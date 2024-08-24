07a75c8e5a302d0e7f368c5e7fb158c849fbcab7
Said Olano <josesaid@gmail.com>
2024-08-24 11:43:58 -0600

    cleanup

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into student values(10001,'Annie', 'E1234567');
-insert into student values(20001,'John', 'A1234568');
-insert into student values(30001,'David','C1232268');
-insert into student values(40001,'Amy','D213458');
